ad1ea64d0d7c3b5b72aa9e6d2aa7d39be5cd8d07
FruitfulEjiro <frutfl@outlook.com>
2025-09-17 14:51:33 +0100

    added solution to Two Sum in Javascript

--- PROBLEM_ONE/two_sum/js/frutfl.js
+++ PROBLEM_ONE/two_sum/js/frutfl.js
@@ -2,31 +2,32 @@ const searchFn = (nums, target) => {
    const map = new Map();
    let result = [];
 
-   for (let i = 0; i < nums.length; i++) {
-      // for (const num of nums) {
-      let pair = target - nums[i];
+   // for (let i = 0; i < nums.length; i++) {
+   for (const num of nums) {
+      let pair = target - num;
       if (map.has(pair)) {
          result.push(map.get(pair));
-         result.push(nums.indexOf(nums[i]));
+         result.push(nums.indexOf(num));
          return result;
       }
 
-      map.set(nums[i], i);
+      // map.set(num, i);
+      map.set(num, nums.indexOf(num));
    }
 
    return result;
 };
 
-const nums = [3, 9, 1, 8, 9, 2, 5, 8, 9, 1, 7, 2, 7, 11, 15]
-const target = 20
+const nums = [3, 9, 1, 8, 9, 2, 5, 8, 9, 1, 7, 2, 7, 11, 15];
+const target = 9;
 const result = searchFn(nums, target);
 
 console.log(result, nums[result[0]] + nums[result[1]]);
 
-
 /*
 
 // My Approach
+
 - first i created a map to store a key value pair of the nums array that i already iterated through in this case i stored the num as the key and its index as the value.
 - before storing i subtract the number from the target to get its possible pair, then check if that pair is in the map.
 - if its not present, i store the current number and its index in the map.
